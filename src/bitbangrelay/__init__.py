3d65dd7ae0700a35b817ff60754e97599da8181c
Paul Barker <paul.barker@sancloud.com>
2022-08-24 13:05:51 +0100

    Release 0.1.0
    Signed-off-by: Paul Barker <paul.barker@sancloud.com>

--- src/bitbangrelay/__init__.py
+++ src/bitbangrelay/__init__.py
@@ -5,4 +5,4 @@ __progname__ = "bitbangrelay"
 __description__ = (
     "Control USB relay boards based on FTDI FT232R/FT245R chips in bitbang mode"
 )
-__version__ = "0.1.0-dev"
+__version__ = "0.1.0"
